cd12dabb48fcfa9673ebbca6267511f7e84e0a0f
Deepak <dk774369@gmail.com>
2024-04-11 20:38:26 +0530

    message attachment

--- components/modals/message-file-modal.tsx
+++ components/modals/message-file-modal.tsx
@@ -2,6 +2,7 @@
 
 import axios from "axios"
 import * as z from 'zod'
+import qs from 'query-string'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
@@ -19,68 +20,67 @@ import {
     FormControl,
     FormField,
     FormItem,
-    FormLabel,
-    FormMessage
 } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { useEffect, useState } from 'react'
 import { FileUpload } from '@/components/file-upload'
 import { useRouter } from "next/navigation"
+import { useModel } from "@/hooks/use-model-store"
 
 const formSchema = z.object({
-    name: z.string().min(1, {
-        message: "Server name is required."
-    }),
-    imageUrl: z.string().min(1, {
-        message: "Server image is required."
+    fileUrl: z.string().min(1, {
+        message: "Attachment is required."
     })
 })
 
-export const InitialModel = () => {
-    const [isMounted, setIsMounted] = useState(false)
-
+export const MessageFileModel = () => {
+    const { isOpen, onClose, type, data } = useModel()
     const router = useRouter()
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
+    const isModalOpen = isOpen && type === 'messageFile'
+    const { apiUrl, query } = data
 
     const form = useForm({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            name: "",
-            imageUrl: "",
+            fileUrl: "",
         }
     })
+
+    const handleClose = () => {
+        form.reset()
+        onClose()
+    }
 
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-            await axios.post('/api/servers', values)
+            const url = qs.stringifyUrl({
+                url: apiUrl ?? "",
+                query,
+            })
+            await axios.post(url, {
+                ...values,
+                content: values.fileUrl,
+            })
 
             form.reset()
             router.refresh()
-            window.location.reload()
+            handleClose()
         } catch (error) {
             console.log(error)
         }
     }
 
-    if(!isMounted){
-        return null
-    }
-
     return(
-        <Dialog open>
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
                 <DialogHeader className='pt-8 px-6'>
                     <DialogTitle className='text-2xl text-center font-bold'>
-                        Customize your Server 
+                        Add an attachment
                     </DialogTitle>
                     <DialogDescription className='text-center text-zinc-500'>
-                        Give your server a personality with a name and an image. You can always change it later.
+                        Send a file as a message
                     </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
@@ -89,12 +89,12 @@ export const InitialModel = () => {
                             <div className='flex items-center justify-center text-center'>
                                 <FormField 
                                     control={form.control}
-                                    name='imageUrl'
+                                    name='fileUrl'
                                     render={({ field }) => (
                                         <FormItem>
                                             <FormControl>
                                                 <FileUpload
-                                                    endpoint="serverImage"
+                                                    endpoint="messageFile"
                                                     value={field.value}
                                                     onChange={field.onChange}
                                                 />
@@ -103,31 +103,10 @@ export const InitialModel = () => {
                                     )}
                                 />
                             </div>
-
-                            <FormField 
-                                control={form.control}
-                                name='name'
-                                render={({ field}) => (
-                                    <FormItem>
-                                        <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70'>
-                                            Server name
-                                        </FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                disabled={isLoading}
-                                                className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
-                                                placeholder='Enter Server name'
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
                         </div>
                         <DialogFooter className='bg-gray-100 px-6 py-4'>
                             <Button variant="primary" disabled={isLoading}>
-                                Create
+                                Send 
                             </Button>
                         </DialogFooter>
                     </form>
