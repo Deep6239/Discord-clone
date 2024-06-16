28dc2025a626043adc34b9817763514a65634554
Deepak <dk774369@gmail.com>
2024-06-16 19:20:01 +0530

    fixed chat-query

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -42,6 +42,7 @@ export const useChatQuery = ({
         queryKey: [queryKey],
         queryFn: fetchMessages,
         getNextPageParam: (lastPage) => lastPage?.nextCursor,
+        initialPageParam: undefined,
         refetchInterval: isConnected ? false : 1000
     })
 
